72bb6383c793fb3aa416febe7f4290400fb6f463
CPedersen3245 <campbelljip@gmail.com>
2017-03-06 17:15:52 +0800

    Added support for multiple gallery pages, still need to work on buttons to change galleries

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
-    //*****Code for fixing keyboard reize issue on mobile*****//
-    $(window).bind('resize', function(e){
+    //*****Code for fixing keyboard resize issue on mobile*****//
+    $(window).bind('resize', function (e) {
         $('body').css('height', '100vh');
         $('body').css('width', '100vw');
     });
@@ -24,6 +24,66 @@ $(document).ready(function () {
         });
     }
 
+    //*****Code for Lightbox2 image gallery*****//
+    //*****Image setup*****//
+    var pathArray = ["static/images/sample/1.jpg", "static/images/sample/2.jpg", "static/images/sample/3.jpg", "static/images/sample/4.jpg",
+        "static/images/sample/5.jpg", "static/images/sample/6.jpg", "static/images/sample/7.jpg", "static/images/sample/8.jpg",
+        "static/images/sample/9.jpg", "static/images/sample/10.jpg", "static/images/sample/11.jpg", "static/images/sample/12.jpg",
+        "static/images/sample/13.jpg", "static/images/sample/14.jpg", "static/images/sample/15.jpg", "static/images/sample/16.jpg",
+        "static/images/sample/17.jpg", "static/images/sample/18.jpg", "static/images/sample/19.jpg", "static/images/sample/20.jpg",
+        "static/images/sample/21.jpg", "static/images/sample/22.jpg", "static/images/sample/23.jpg", "static/images/sample/24.jpg",
+        "static/images/sample/1.jpg", "static/images/sample/2.jpg", "static/images/sample/3.jpg", "static/images/sample/4.jpg",
+        "static/images/sample/5.jpg", "static/images/sample/6.jpg", "static/images/sample/7.jpg", "static/images/sample/8.jpg",
+        "static/images/sample/9.jpg", "static/images/sample/10.jpg", "static/images/sample/11.jpg", "static/images/sample/12.jpg",
+        "static/images/sample/13.jpg", "static/images/sample/14.jpg", "static/images/sample/15.jpg", "static/images/sample/16.jpg",
+        "static/images/sample/17.jpg", "static/images/sample/18.jpg", "static/images/sample/19.jpg", "static/images/sample/20.jpg",
+        "static/images/sample/21.jpg", "static/images/sample/22.jpg", "static/images/sample/23.jpg", "static/images/sample/24.jpg"];
+
+    //Code for scrolling between photo lists
+    var photoLists = [];
+    var currPhotoListIndex = 0;
+
+    //*****HTML Writing*****//
+    //For each x photos, make a new photo area
+    var x = 20;
+    var i, j = 0;
+    var listnumber = 0;
+    var length = pathArray.length;
+
+    while (j < length) {
+        photoLists.push("GPA" + listnumber);
+        console.log(photoLists);
+        $(".gallery-slider").append(
+                '<div class="gallery-photo-area" id="GPA' + listnumber + '">' +
+                    '<ul id="photo-list-' + listnumber + '"></ul>'
+        );
+        for (i = 0; i < x; i++) {
+            if(j < length) {
+                $("#photo-list-" + listnumber).append(
+                    '<li>' +
+                        '<a href=' + pathArray[j] + ' data-lightbox="gallery">' +
+                            '<img src=' + pathArray[j] + '>' +
+                        '</a>' +
+                    '</li>'
+                );
+                j++;
+            }
+        }
+        $(".gallery-w-padding").append(
+            '</div>'
+        );
+        listnumber++;
+    }
+
+    //*****CSS Writing*****//
+    function initCSS() {
+        $(".gallery-photo-area").css('width', ($(".gallery-w-padding").width() + 'px'));
+    }
+    initCSS();
+
+    $(window).resize(initCSS);
+
+
     //*****Datepicker setup*****//
     $('#gallery-start-date-selector').datetimepicker({
         controlType: 'select',
@@ -32,25 +92,6 @@ $(document).ready(function () {
     $('#gallery-end-date-selector').datetimepicker({
         controlType: 'select',
         oneLine: true,
-    });
-
-    //*****Code for Lightbox2 image gallery*****//
-    //*****Image setup*****//
-    var pathArray = ["static/images/sample/1.jpg", "static/images/sample/2.jpg", "static/images/sample/3.jpg", "static/images/sample/4.jpg",
-        "static/images/sample/5.jpg", "static/images/sample/6.jpg", "static/images/sample/7.jpg", "static/images/sample/8.jpg",
-        "static/images/sample/9.jpg", "static/images/sample/10.jpg", "static/images/sample/11.jpg", "static/images/sample/12.jpg",
-        "static/images/sample/13.jpg", "static/images/sample/14.jpg", "static/images/sample/15.jpg", "static/images/sample/16.jpg",
-        "static/images/sample/17.jpg", "static/images/sample/18.jpg", "static/images/sample/19.jpg", "static/images/sample/20.jpg",
-        "static/images/sample/21.jpg", "static/images/sample/22.jpg", "static/images/sample/23.jpg", "static/images/sample/24.jpg"];
-
-    $.each(pathArray, function (index, value) {
-        $("#photo-list").append(
-            '<li>' +
-                '<a href=' + value + ' data-lightbox="gallery">' +
-                    '<img src=' + value + '>' +
-                '</a>' +
-            '</li>'
-        );
     });
 
     //Configuring Lightbox options
@@ -76,7 +117,7 @@ $(document).ready(function () {
 
     //*****Error handling*****//
     function picturesError(jqXHR, status, errorThrown) {
-        alert(jqXHR.responseText)
+        alert(jqXHR.responseText);
     }
 
 });
